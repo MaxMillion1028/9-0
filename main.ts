d2bdffd8756a7ada56e9857aba8e4957e9dedc84
MaxMillion1028 <72779441+MaxMillion1028@users.noreply.github.com>
2020-10-13 12:01:14 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.forever(function () {
-    for (let x = 0; x <= 9; x++) {
-        for (let y = 0; y <= 9; y++) {
-            basic.showString("" + x + "x" + y + "=" + x * y)
+    for (let x = 0; x <= 8; x++) {
+        for (let y = 0; y <= 8; y++) {
+            basic.showString("" + (x + 1) + "x" + (y + 1) + "=" + (x + 1) * (y + 1))
         }
     }
 })
